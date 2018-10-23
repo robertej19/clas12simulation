369aaf3ca59c8ca2faef29b64ae107c26e544ba2
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2018-10-23 16:11:44 -0400

    fixed input file lines, comment was misparsed

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/csh
 
+# initial job log
+printf "Start time: "; /bin/date
+printf "Job is running on node: "; /bin/hostname
+printf "Job running as user: "; /usr/bin/id
+printf "Job is running in directory: "; /bin/pwd
+
+
 pwd
 
 ls -l
@@ -11,4 +18,8 @@ which gemc
 gemc -help-all
 
 
+gemc clas12.gcard -N=100 -PRINT_EVENT=10 -USE_GUI=0
 
+
+# final job log
+printf "Job finished time: "; /bin/date
