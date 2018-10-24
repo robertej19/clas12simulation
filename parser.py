b189f095da273b81727c7e01d7399c6265689c75
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2018-10-24 11:00:59 -0400

    starting parsing arguments

--- parser.py
+++ parser.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python
 
+import os
+import sys
+
+# command line call example:
+# python -c 'import commonRoot ; commonRoot.commonFilenameRoot("list.txt")'
+def commonFilenameRoot(filename = ''):
+	print filename
+
+
+#a = sys.argv[1]
+
+#print a
+
+#f = open('list.txt', 'r')
+#x = f.readlines()
+#f.close()
+
+
+#for file in x:
+#print file
+
+#c = os.path.commonprefix(x)
+
+#print c
--- run.sh
+++ run.sh
@@ -1,24 +1,14 @@
 #!/bin/csh
 
 # initial job log
+
 printf "Start time: "; /bin/date
 printf "Job is running on node: "; /bin/hostname
 printf "Job running as user: "; /usr/bin/id
 printf "Job is running in directory: "; /bin/pwd
 
 
-pwd
-
-ls -l
-ls -l /jlab
-ls -l /jlab/work
-
-which gemc
-
 gemc -help-all
-
-
-gemc clas12.gcard -N=100 -PRINT_EVENT=10 -USE_GUI=0
 
 
 # final job log
