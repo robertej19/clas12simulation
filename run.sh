6f90973dc747cd78d09f884c458a726e95145275
Maurizio Ungaro <maurizio.ungaro@cnu.edu>
2018-10-31 15:54:46 -0400

    logging each gemc, conversion, cooking process

--- run.sh
+++ run.sh
@@ -11,23 +11,25 @@ printf "Job is running in directory: "; /bin/pwd
 echo starting files
 ls -l
 
-gemc -USE_GUI=0 -N=1000 -BEAM_P="e-, 4*GeV, 20*deg, 5*deg" clas12.gcard
+rm -f gemc.log
+gemc -USE_GUI=0 -N=1000 -BEAM_P="e-, 4*GeV, 20*deg, 5*deg" clas12.gcard > gemc.log
 
 echo after gemc
 ls -l
 
-evio2hipo -r 11 -t -1.0 -s -1.0 -i out.ev -o gemc.hipo
+rm -f e2h.log
+evio2hipo -r 11 -t -1.0 -s -1.0 -i out.ev -o gemc.hipo > e2h.log
 
 echo after evio2hipo
 ls -l
 
-cook.csh
+rm -f reco.log
+cook.csh > reco.log
 
 cat cook.clara
 
 echo after cooking
 ls -l
-
 
 # final job log
 printf "Job finished time: "; /bin/date
