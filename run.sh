912fb6247178d86f243b4f70e9bb153f3a88b8e1
maureeungaro <maurizio.ungaro@cnu.edu>
2018-10-31 16:54:26 -0400

    succesful test

--- run.sh
+++ run.sh
@@ -12,24 +12,32 @@ echo starting files
 ls -l
 
 rm -f gemc.log
-gemc -USE_GUI=0 -N=1000 -BEAM_P="e-, 4*GeV, 20*deg, 5*deg" clas12.gcard > gemc.log
+gemc -USE_GUI=0 -N=100 -BEAM_P="e-, 4*GeV, 20*deg, 5*deg" clas12.gcard > gemc.log
 
 echo after gemc
 ls -l
+echo
+
+
 
 rm -f e2h.log
 evio2hipo -r 11 -t -1.0 -s -1.0 -i out.ev -o gemc.hipo > e2h.log
 
 echo after evio2hipo
 ls -l
+echo
+
+
 
 rm -f reco.log
-cook.csh > reco.log
-
+csh ./cook.csh > reco.log
 cat cook.clara
 
 echo after cooking
 ls -l
+echo
+
+
 
 # final job log
 printf "Job finished time: "; /bin/date
