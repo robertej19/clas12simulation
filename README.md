88b350512d781a1f71f6f360224c31fe69abf412
Sangbaek Lee <sangbaek@submit.mit.edu>
2019-03-02 21:19:02 -0500

    the most updated versions

--- README.md
+++ README.md
@@ -1,3 +1,91 @@
+# scard.txt
+
+The first row 'project' was changed to 'group' because there is one more project at scard. If scard is in different format, parser.py will print ERROR and halt.
+
+# parser.py
+parser.py is a module (like a header file in C) that parse scard.
+Every information from parser should be stored in a class scard_parser
+
+# submit.py
+submit.py is a main python script that calls parser.py.
+It overwrites clas12.condor and runscript.sh. The .condor and .sh scripts should contain the info from scard.txt
+ 
+# clas12.condor
+condor_submit clas12.condor will submit a job.
+
+# run_job.sh
+an executable called by clas12.condor. It will call condor_wrapper.
+
+# condor_wrapper
+a script called by run_job.sh. It will call runscript.sh
+
+# runscript.sh
+This is a main script which calls generator, GEMC, decoder, and reconstruction. submit.py will update runscript.sh regarding scard.txt
+
+# Generator Option
+
+To see genOptions, please look at followings:<br />
+SIDIS: https://github.com/JeffersonLab/clasdis-nocernlib/blob/master/README.md<br />
+DVCS: https://github.com/JeffersonLab/dvcsgen/blob/master/README.md<br />
+DIS-RAD: https://github.com/JeffersonLab/inclusive-dis-rad/blob/master/README.md
+
+# condorHelp
+Quickstart: https://support.opensciencegrid.org/support/solutions/articles/5000633410-osg-connect-quickstart
+
+
+To submit a job:
+----------------
+
+condor_submit condorTemplate.submit
+
+
+Troubleshooting:
+
+ condor_q -better-analyze JOBID
+
+
+History:
+--------
+
+condor_history mauri
+
+for details:
+
+condor_history -long mauri
+
+condor_q - Lists the jobs in the queue. Can be invoked with your username:
+
+condor_q mauri
+
+
+
+https://support.opensciencegrid.org/support/solutions/articles/5000623439-osg-xsede-user-guide
+
+
+Guidelines for data management: https://support.opensciencegrid.org/support/solutions/articles/12000006512-guidelines-for-data-managment-in-osg-storage-and-transfer<br />
+OSG Helpdesk: https://support.opensciencegrid.org/support/home
+
+HTCondor: https://research.cs.wisc.edu/htcondor/index.html
+
+Condor Documentation: https://research.cs.wisc.edu/htcondor/manual/
+
+
+To run the singularity container:
+
+singularity shell --home ${PWD}:/srv --pwd /srv --bind /cvmfs --contain --ipc --pid /cvmfs/singularity.opensciencegrid.org/maureeungaro/clas12simulations:production
+
+
+Managing Jobs:
+-------------
+
+On hold:
+
+condor_q  -hold
+
+Removing a job (by ID)
+
+condor_rm ID
+
 # clas12 Simulations Software Distribution
 
 
@@ -17,5 +105,4 @@ Any changes to the docker image in the hub.docker.com repository will trigger th
 The image is loaded in:
 
 ```/cvmfs/singularity.opensciencegrid.org/maureeungaro/```
-
 
--- parser.py
+++ parser.py
@@ -1,73 +1,109 @@
-#!/usr/bin/python
+import argparse, sys, os, subprocess
 
-import os
-import sys
+# declare a global dictionary to match genOutput and genExecutable to generator row
+genOutput= {'clasdis': 'sidis.dat', 'dvcs': 'dvcs.dat','disrad':'dis-rad.dat'}
+genExecutable =  {'clasdis': 'clasdis', 'dvcs': 'dvcsgen','disrad':'generate-dis'}
 
-#
-
-# command line call example: python -c 'import parser ; parser.commonFilenameRoot("/Users/ungaro/list.txt")'
-def commonFilenameRoot(filename = ''):
-	print "Importing list of files from: ", filename
-	f = open(filename, 'r')
-	files = f.readlines()
-	f.close()
-	common = os.path.commonprefix(files)
-	print "Common path: ", common
+# Proper configuration of scard:
+scard_key = ['group','user','nevents','generator', 'genOptions',  'gcards', 'jobs',  'project', 'luminosity', 'tcurrent',  'pcurrent']
 
 
-# command line call example: python -c 'import parser ; parser.getOption("scard.txt")'
-def getOption(filename = 'scard.txt'):
-	print "Importing list of files from: ", filename
+# from https://codegolf.stackexchange.com/questions/4707/outputting-ordinal-numbers-1st-2nd-3rd#answer-4712
+def ordinal(n):
+    return "%d%s" % (n,"tsnrhtdd"[(n/10%10!=1)*(n%10<4)*n%10::4])
 
+class scard_parser:
+# Default Constructor: a = scard_parser();  a.parse_scard("scard.txt")
+# Parametrized Constructor:  a = scard_parser("scard.txt")
+    def __init__(self, scard_filename=None):
+        self.type ='scard parser'
+        self.data = {}
+        if scard_filename != None:
+            self.parse_scard(scard_filename)
 
+# void function for saving line into a dictionary
+    def parse_scard_line(self, linenum, line):
+        self.validate_scard_line(linenum, line) # 1st validating
+        pos_delimeter_colon = line.find(":")
+        pos_delimeter_hash = line.find("#")
+        key =   line[:pos_delimeter_colon].strip()
+        value=  line[pos_delimeter_colon+1:pos_delimeter_hash].strip()
+        if key != scard_key[linenum]:
+            print "ERROR: The " + ordinal(linenum+1) +" line of steering card starts with "+ key +"."
+            print "That line must have the key " + scard_key[linenum] +"."
+        self.data[key] = value
 
-# arguments: filename and string to find
-filename = sys.argv[1]
-toFind   = sys.argv[2]
+# voild function for parsing scard.txt into a dictionary
+    def parse_scard(self, filename, store=True):
+        scard=open(filename, "r")
+        for linenum, line in enumerate(scard):
+            self.parse_scard_line(linenum,line)
+        if store == True:
+            self.store()
 
-lines = []             # Declare an empty list named "lines"
+#void function for validating s_card
+    def validate_scard_line(self, linenum, line):
+        if line.count("#") ==0:
+            print "Warning: No comment in "+ ordinal(linenum+1) + " line."
+        elif line.count("#")>1:
+            print "ERROR: number of hashes>1 at "+ordinal(linenum+1)+" line."
+            print "# can be only used for a delimeter and for once. Stopped."
+            exit()
+        if line.count(":") ==0:
+            print "ERROR: No colon in "+ ordinal(linenum+1) + " line."
+            print "The data cannot be interpreted. Stopped."
+            exit()
+        elif line.count(":")>1:
+            print "ERROR: number of colons>1 at "+ordinal(linenum+1)+" line."
+            print "\':\' can be only used for a delimeter and for once. Stopped."
+            exit()
+# store info's in dictionary into single variables
+    def store(self):
+        self.group = self.data.get("group")
+        self.user = self.data.get("user")
+        self.nevents = self.data.get("nevents")
+        self.generator = self.data.get("generator")
+        self.genOptions = self.data.get("genOptions")
+        self.gcards = self.data.get("gcards")
+        self.jobs = self.data.get("jobs")
+        self.project = self.data.get("project")
+        self.luminosity = self.data.get("luminosity")
+        self.tcurrent = self.data.get("tcurrent")
+        self.pcurrent = self.data.get("pcurrent")
+        self.genOutput = genOutput.get(self.data.get("generator"))
+        self.genExecutable = genExecutable.get(self.data.get("generator"))
 
-if not "output" in toFind:
-	with open (filename, 'rt') as in_file:  # Open file
-		in_file.seek(0)
-		for line in in_file:      # For each line of text in in_file, where the data is named "line",
-			if toFind in line:     # match toFInd string
-				lines.append(line)  # add that line to our list of lines.
+def write_clas12_condor(project, jobs):
+    file_template = open("clas12.condor.template","r")
+    str_template = file_template.read()
+    file_template.close()
+    str_script=str_template.replace('project_scard', project)
+    str_script=str_script.replace('jobs_scard', jobs)
+    print "overwrite \'clas12.condor\' in current directory ..."
+    file = open("clas12.condor","w")
+    file.write(str_script)
+    file.close()
+    print "Done.\n"
 
-	for element in lines:                 # For each element in our list
-		start = element.find(":") + 1      # after the :
-		end   = element.find("#")          # before #
-		found = element[start:end].strip() # stripping whitespaces
-		print(found)
+def write_runscirpt_sh(group, user, genExecutable, nevents, genOptions, genOutput, gcards, tcurrent, pcurrent):
+    file_template = open("runscript.sh.template","r")
+    str_template = file_template.read()
+    file_template.close()
+    str_script=str_template.replace('group_scard', group)
+    str_script=str_script.replace('user_scard',user)
+    str_script=str_script.replace('genExecutable_scard', genExecutable)
+    str_script=str_script.replace('nevents_scard', nevents)
+    str_script=str_script.replace('genOptions_scard', genOptions)
+    str_script=str_script.replace('genOutput_scard', genOutput)
+    str_script=str_script.replace('gcards_scard', gcards)
+    str_script=str_script.replace('tcurrent_scard', tcurrent)
+    str_script=str_script.replace('pcurrent_scard', pcurrent)
+    print "overwrite \'runscript.sh\' in current directory ..."
+    file = open("runscript.sh","w")
+    file.write(str_script)
+    file.close()
+    print "Done.\n"
 
-if "genOutput" in toFind:
-	with open (filename, 'rt') as in_file:  # Open file
-		for line in in_file:          # For each line of text in in_file, where the data is named "line",
-			if "generator:" in line:  # match generator to find type
-				lines.append(line)     # add that line to our list of lines.
-	for element in lines:                 # For each element in our list
-		start = element.find(":") + 1      # after the :
-		end   = element.find("#")          # before #
-		found = element[start:end].strip() # stripping whitespaces
-	if "clasdis" in found != -1:
-		print "sidis.dat"
-	elif "dvcs" in found != -1:
-		print "dvcs.dat"
-	elif "disrad" in found != -1:
-		print "dis-rad.dat"
-
-if "genExecutable" in toFind:
-	with open (filename, 'rt') as in_file:  # Open file
-		for line in in_file:          # For each line of text in in_file, where the data is named "line",
-			if "generator:" in line:  # match generator to find type
-				lines.append(line)     # add that line to our list of lines.
-	for element in lines:                 # For each element in our list
-		start = element.find(":") + 1      # after the :
-		end   = element.find("#")          # before #
-		found = element[start:end].strip() # stripping whitespaces
-	if "clasdis" in found != -1:
-		print "clasdis"
-	elif "dvcs" in found != -1:
-		print "dvcsgen"
-	elif "disrad" in found != -1:
-		print "generate-dis"
+def condor_submit():
+    print "submitting jobs from python script...\n"
+    subprocess.call(["condor_submit","clas12.condor"])
