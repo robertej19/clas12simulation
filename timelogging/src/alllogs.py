ca2685179db75be4b83403f91c1a5e6540de36ec
robertej19 <robertej@mit.edu>
2019-03-04 09:48:11 -0500

    organized file structure

--- timelogging/src/alllogs.py
+++ timelogging/src/alllogs.py
@@ -5,13 +5,13 @@ import os
 
 
 def jobstowrite():
-  mypath = os.path.dirname(os.path.realpath(__file__))+'/log'
+  mypath = os.path.dirname(os.path.realpath(__file__))+'/../../log'
   files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
   outfiles = [ x for x in files if "out" in x]
   return outfiles
 
 for job in jobstowrite():
-  if os.stat('log/'+job).st_size == 0:
+  if os.stat('../../log/'+job).st_size == 0:
     print job + " is empty. Skipping. \n"
   else:
     write_record(job)
