75f3e9babd1ac8bc3f8b75ff697ed8b933234988
robertej19 <robertej@mit.edu>
2019-03-04 00:31:09 -0500

    adding reading from DB, basic plotting. Need to add Event marker in DB

--- log_write.py
+++ log_write.py
@@ -2,10 +2,11 @@ from timeparser import *
 
 import sqlite3
 
-conn = sqlite3.connect('TimeLog.db')
-c = conn.cursor()
+
 
 def dynamic_data_entry(jobnumber,timearray):
+    conn = sqlite3.connect('TimeLog.db')
+    c = conn.cursor()
     ta = timearray
     c.execute("INSERT INTO JobLog(jobnumber, initialization, generation, gemc, evio2hipo, reconstruction, total) VALUES (?,?,?,?,?,?,?)",
               (jobnumber,ta[0],ta[1],ta[2],ta[3],ta[4],ta[5]))
--- make_log_db.py
+++ make_log_db.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect('TimeLog.db')
 c = conn.cursor()
 
 def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS JobLog(jobnumber TEXT, initialization INT,generation INT,gemc INT,evio2hipo INT,reconstruction INT,total INT)')
+    c.execute('CREATE TABLE IF NOT EXISTS JobLog(jobID integer primary key autoincrement,jobnumber TEXT, initialization INT,generation INT,gemc INT,evio2hipo INT,reconstruction INT,total INT)')
 
 create_table()
 
