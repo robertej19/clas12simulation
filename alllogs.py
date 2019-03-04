2fa54aa7e135cac96be3bfb32a3b44fb5b29561b
robertej19 <robertej@mit.edu>
2019-03-03 23:50:53 -0500

    writes entire log directory to database

---
+++ alllogs.py
@@ -0,0 +1,17 @@
+from log_write import *
+from os import listdir
+from os.path import isfile, join
+import os
+
+
+def jobstowrite():
+  mypath = os.path.dirname(os.path.realpath(__file__))+'/log'
+  files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+  outfiles = [ x for x in files if "out" in x]
+  return outfiles
+
+for job in jobstowrite():
+  if os.stat('log/'+job).st_size == 0:
+    print job + " is empty. Skipping. \n"
+  else:
+    write_record(job)
--- log_write.py
+++ log_write.py
@@ -1,7 +1,5 @@
 from timeparser import *
 
-jobnumber = '7299011.7'
-
 import sqlite3
 
 conn = sqlite3.connect('TimeLog.db')
@@ -12,8 +10,9 @@ def dynamic_data_entry(jobnumber,timearray):
     c.execute("INSERT INTO JobLog(jobnumber, initialization, generation, gemc, evio2hipo, reconstruction, total) VALUES (?,?,?,?,?,?,?)",
               (jobnumber,ta[0],ta[1],ta[2],ta[3],ta[4],ta[5]))
     conn.commit()
-    print("Record added to DB")
+    print("Record added to DB from "+jobnumber)
     c.close()
     conn.close()
 
-dynamic_data_entry(jobnumber,parse_times('job.'+jobnumber+'.out'))
+def write_record(jobnumber):
+  dynamic_data_entry(jobnumber,parse_times(jobnumber)) #technically this writes the job.jobnumber.out to the record, but oh well. Will fix later.
