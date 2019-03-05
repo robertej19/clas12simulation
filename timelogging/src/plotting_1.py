a40dde4693e08268319d6aeebeaf1d8a2911c3d7
robertej19 <robertej@mit.edu>
2019-03-04 22:49:30 -0500

    added pie chart plotting

--- timelogging/src/plotting_1.py
+++ timelogging/src/plotting_1.py
@@ -11,11 +11,11 @@ def grab_color():
     return nevents
 
 def grab_data():
-    c.execute('SELECT initialization, generation, gemc, evio2hipo, reconstruction, total FROM JobLog')
+    c.execute('SELECT initialization, generation, gemc, evio2hipo, reconstruction FROM JobLog')
     dba = list(c.fetchall())
     return dba
 
-comps = ['initialization', 'generation', 'gemc', 'evio2hipo', 'reconstruction', 'total']
+comps = ['initialization', 'generation', 'gemc', 'evio2hipo', 'reconstruction','completion']
 sv = grab_data()
 colorbook = grab_color()
 #print " number of events: {0} \n number of jobs: {1}".format(scard_values[1],scard_values[2])
@@ -23,8 +23,18 @@ colorbook = grab_color()
 colors = ['k','r','b']
 colornum = [1000,2000,4000]
 
+init=[]
+gen = []
+gemc = []
+evio = []
+recon = []
 for i in range(0,49):
-  y = sv[i]
+  init.append(sv[i][0])
+  gen.append(sv[i][1])
+  gemc.append(sv[i][2])
+  evio.append(sv[i][3])
+  recon.append(sv[i][4])
+  y = (0,)+sv[i]
   line_color = colors[colornum.index(colorbook[i][0])]
   print(y)
   z = tuple([x/3600 for x in y])
@@ -41,4 +51,22 @@ plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
 #plt.legend(loc="best")
 plt.tight_layout()
+plt.show()
+
+def avs(array):
+  ave = sum(array)/len(array)
+  return ave
+
+labels = comps[:-1]
+sizes = [avs(init),avs(gen),avs(gemc),avs(evio),avs(recon)]
+colors = ['black','gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0,0,0,0,0)  # explode 1st slice
+
+# Plot
+#plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+#autopct='%1.1f%%', shadow=True, startangle=140)
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+autopct='%1.1f%%')
+
+plt.axis('equal')
 plt.show()
