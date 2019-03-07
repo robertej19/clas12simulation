614f6cef0b0e9264eb89eb52595b7e62910176d8
Sangbaek <sangbaek@mit.edu>
2019-03-07 12:33:47 -0500

    gemc now takes luminosity

--- submit.py
+++ submit.py
@@ -24,10 +24,10 @@ genOptions = scard.genOptions
 gcards = scard.gcards
 jobs = scard.jobs
 project = scard.project
-luminosity =  '%1.2f'(124000 * float(scard.luminosity))
+luminosity =  '%1.2f'%(124000 * float(scard.luminosity))
 if float(scard.luminosity) == 0:
     print "Warning: luminosity is 0. GEMC will run with 100% luminosity."
-    luminosity = 124000
+    luminosity = '124000'
 tcurrent = '%1.2f'%(float(scard.tcurrent)/100.)
 if float(scard.tcurrent)%10==0:
     tcurrent = '%1.1f'%(float(scard.tcurrent)/100.)
