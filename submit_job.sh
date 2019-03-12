e5c48ff0b1ceea8f26ab31920aa67b0cf7ca6796
robertej19 <robertej@mit.edu>
2019-03-12 17:38:52 -0400

    fixed minor typos

--- submit_job.sh
+++ submit_job.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
+
+if [ ! -f scard.txt ]; then
+	echo "scard.txt not found, please create before attempting to continue. Exiting"
+	exit
+fi
+cp scard.txt src/
+
+cd src
 ls
-#cd /src/
-#if [ ! -f /tmp/foo.txt ]; then
-#    echo "CLAS12 Off Campus Resources Database not found, creating!"
-#    python2 make_DB.py
-#fi
-#python2 SCard_Table_Writer.py
+
+if [ ! -f CLAS12_OCRDB.db ]; then
+	echo "CLAS12 Off Campus Resources Database not found, creating!"
+	python2 make_DB.py
+fi
+
+python2 Users_Table_Writer.py
+
+echo "Reading scard into database"
+python2 SCard_Table_Writer.py
+
+rm scard.txt
+
+echo "Writing Scard information to submission scripts"
+python2 SCard_Table_Parser.py
+
+
+cd ../
+
+mv src/runscript.sh .
+mv src/clas12.condor .
+
+condor_submit clas12.condor
