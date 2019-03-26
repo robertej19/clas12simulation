a908df5cf0d6ae9a657c3311de36e185cc2cf5db
robertej19 <robertej@mit.edu>
2019-03-26 12:42:20 -0400

    Foreign Key Relations Functional

--- src/SCard_Table_Writer.py
+++ src/SCard_Table_Writer.py
@@ -16,6 +16,7 @@ scard_fields.data['group_name'] = scard_fields.data.pop('group') #'group' is a p
 scard_fields.data['genExecutable'] = file_struct.genExecutable.get(scard_fields.data.get('generator'))
 scard_fields.data['genOutput'] = file_struct.genOutput.get(scard_fields.data.get('generator'))
 
-UID = "robertej" #This is more or less a placeholder, currently. Explained in 20190312 PR documentation
+UID = 1 #This 1 references the first USER listed in the USERS table of the database. This User must exist, or else an error will be thrown.
+#This above (UID) is more or less a placeholder, currently. Explained in 20190312 PR documentation
 unixtimestamp = int(time.time()) # Can modify this if need 10ths of seconds or more resolution
 SCard_Entry(file_struct.DBname,UID,unixtimestamp,scard_fields.data)
