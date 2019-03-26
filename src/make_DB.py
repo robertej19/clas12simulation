8bcade3d3a2aab88dbb09285b582858b37585bcb
robertej19 <robertej@mit.edu>
2019-03-26 12:51:58 -0400

    working foreign key relations

--- src/make_DB.py
+++ src/make_DB.py
@@ -5,18 +5,12 @@ import sqlite3, utils, file_struct
 tables = ['Users','Scards','JobsLog']
 PKs = ['UserID','BatchID','JobID']
 tab_fields = [file_struct.users_fields,file_struct.scard_fields,file_struct.jobslog_fields]
-FKargs = ["""""",
-          """, UserID INTEGER,
-          FOREIGN KEY(UserID) REFERENCES Users(UserID)""",
-          """, BatchID TEXT, UserID TEXT,
-          FOREIGN KEY(BatchID) REFERENCES Scards(BatchID)
-          FOREIGN KEY(UserID) REFERENCES Users(UserID)"""]
 
 #('BatchID', 'TEXT'),('UserID','TEXT'),
 
 #Create tables in the database
 for i in range(0,len(tables)):
-  utils.create_table(file_struct.DBname,tables[i],PKs[i],FKargs[i])
+  utils.create_table(file_struct.DBname,tables[i],PKs[i],file_struct.foreign_key_relations[i])
 
 #Add fields to each table in the database
 for j in range(0,len(tables)):
