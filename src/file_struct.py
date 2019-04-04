bab6517994397d8162e86165b319aa196e3ed398
robertej19 <robertej@mit.edu>
2019-04-04 00:11:44 -0400

    Revert "writes scard to database"
    This reverts commit 2e0d038a948138b9cd0484ae3eed168cb1f31c40.

--- src/file_struct.py
+++ src/file_struct.py
@@ -27,26 +27,7 @@ scard_fields = (('group_name','TEXT'),('User','TEXT'),('Nevents','INT'),
                 ('Generator','TEXT'),('genExecutable','TEXT'),('genOutput','TEXT'),
                 ('GenOptions','TEXT'),('Gcards','TEXT'),('Jobs','INT'),
                 ('Project','TEXT'),('Luminosity','INT'),('Tcurrent','INT'),('Pcurrent','INT'),
-                ('Cores_Req','INT'),('Mem_Req','INT'),('timestamp','FLOAT'),('scard','VARCHAR'))
-"""
-batches_fields = (batch id, userid FK, user, datestamp, scard, running pool, runscripts, gcard
-
-gcard_fields = (gcard id, batchid fk)
-
-group:  rgaDIS                   # project description
-user:  mungaro                   # username
-nevents: 100                     # number of events each job
-generator: clasdis               # one of clasdis, dvcsgen, generate-dis
-genOptions:  --t 15 20           # generator option
-gcards: /jlab/work/clas12.gcard  # gcard
-jobs: 1                          # number of jobs
-project:  osg.clas12MC           # OSG project
-luminosity:  0                   # percent of 10^35 luminosity from 0 to 100
-tcurrent: -100                   # percent of torus current from -100 to 100
-pcurrent: -100                   # percent of solenoid current from -100 to 100
-cores_req: 1                     # number of cores to request from node
-mem_req: 2                       # GB of RAM to request from node. Currently set to 4 for testing purposes
-"""
+                ('Cores_Req','INT'),('Mem_Req','INT'),('timestamp','FLOAT'))
 
 jobslog_fields = (('Job_Submission_Datestamp','INT'),
                   ('Job_Completion_Datestamp','TEXT'),('Output_file_directory','TEXT'),
