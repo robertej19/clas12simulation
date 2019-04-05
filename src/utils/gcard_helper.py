af5050a9724b0c145280ba266a48185f0499abd7
robertej19 <robertej@mit.edu>
2019-04-05 06:12:07 -0400

    Gcards almost working

--- src/utils/gcard_helper.py
+++ src/utils/gcard_helper.py
@@ -7,14 +7,31 @@ import urllib2
 #>>> resp = urllib2.urlopen("http://google.com/abc.jpg")
 
 def GCard_Entry(DBname,UID,BatchID,unixtimestamp,url_dir):
+  gcard_urls = Gather_Gcard_urls(url_dir)
+  for url_ending in gcard_urls:
+    response = urllib2.urlopen(url_dir+'/'+url_ending)
+    gcard_text = response.read()
+    db_gcard_write(DBname,UID,BatchID,unixtimestamp,gcard_text)
+
+def db_gcard_write(DBname,UID,BatchID,timestamp,gcard_text):
+    strn = "INSERT INTO Gcards(UserID,timestamp) VALUES ('{0}',{1});".format(UID,timestamp)
+    utils.sql3_exec(DBname,strn)
+    strn = "UPDATE Gcards SET {0} = '{1}' WHERE timestamp = {2};".format('Gcards',gcard_text,timestamp)
+    utils.sql3_exec(DBname,strn)
+    print("GCard record added to database corresponding to BatchID {}".format(BatchID))
+    strn = "UPDATE Gcards SET {0} = '{1}' WHERE timestamp = {2};".format("BatchID",BatchID,timestamp)
+    utils.sql3_exec(DBname,strn)
+
+def Gather_Gcard_urls(url_dir):
+  gcard_urls = []
   response = urllib2.urlopen(url_dir)
-  html = response.read()
-  a = ".gcard"
-  bb = ">"
-  q = html.split(' ')
-  for item in q:
-    if a in item:
-      b = item.split('"')
-      for item in b:
-        if (a in item) and (bb not in item):
-          print(item)
+  html = response.read().split(' ')
+  matching_text = ".gcard"
+  second_qualifier = ">"
+  for section in html:
+    if matching_text in section:
+      sections = section.split('"')
+      for item in sections:
+        if (matching_text in item) and (second_qualifier not in item):
+          gcard_urls.append(item)
+  return gcard_urls
